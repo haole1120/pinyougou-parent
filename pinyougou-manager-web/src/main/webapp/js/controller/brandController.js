60f7e71f071d86d84884932c74533837a8746900
haole <haole@haole.com>
2020-11-13 16:13:58 +0800

    代码分离JS,控制器

--- pinyougou-manager-web/src/main/webapp/js/controller/brandController.js
+++ pinyougou-manager-web/src/main/webapp/js/controller/brandController.js
@@ -1,4 +1,7 @@
-app.controller('brandController', function ($scope, $http, brandService) {
+app.controller('brandController', function ($scope, $controller,brandService) {
+
+    $controller('baseController',{$scope:$scope});
+
     //查询品牌列表
     $scope.findAll = function () {
         brandService.findAll().success(
@@ -10,22 +13,7 @@ app.controller('brandController', function ($scope, $http, brandService) {
 
     }
 
-    //分页控件配置
-    $scope.paginationConf = {
-        currentPage: 1,
-        totalItems: 10,
-        itemsPerPage: 10,
-        perPageOptions: [10, 20, 30, 40, 50],
-        onChange: function () {
-            //$scope.findPage($scope.paginationConf.currentPage,$scope.paginationConf.itemsPerPage);//太长，封装进方法reloadList
-            $scope.reloadList();//重新加载
-        }
-    };
 
-    //刷新列表
-    $scope.reloadList = function () {
-        $scope.search($scope.paginationConf.currentPage, $scope.paginationConf.itemsPerPage);
-    }
 
     //分页
     $scope.findPage = function (page, rows) {
@@ -69,16 +57,7 @@ app.controller('brandController', function ($scope, $http, brandService) {
 
     $scope.selectIds = [];//用户勾选的ID集合
 
-    //用户勾选复选框
-    $scope.updateSelection = function ($event, id) {
-        if ($event.target.checked) {
-            $scope.selectIds.push(id);//push方法向集合添加元素
 
-        } else {
-            var index = $scope.selectIds.indexOf(id);//查找值的位置
-            $scope.selectIds.splice(index, 1);//参数1：移除的位置 参数2：移除的个数
-        }
-    }
 
     //删除
     $scope.dele = function () {
