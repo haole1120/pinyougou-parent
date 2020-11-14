ba537932e4fabdef741d75f1537eea4fe295837b
haole <haole@haole.com>
2020-11-14 20:06:34 +0800

    商家入驻审核

---
+++ pinyougou-shop-web/src/main/webapp/js/controller/baseController.js
@@ -0,0 +1,49 @@
+app.controller('baseController', function ($scope) {
+    //分页控件配置
+    $scope.paginationConf = {
+        currentPage: 1,
+        totalItems: 10,
+        itemsPerPage: 10,
+        perPageOptions: [10, 20, 30, 40, 50],
+        onChange: function () {
+            //$scope.findPage($scope.paginationConf.currentPage,$scope.paginationConf.itemsPerPage);//太长，封装进方法reloadList
+            $scope.reloadList();//重新加载
+        }
+    };
+
+    //刷新列表
+    $scope.reloadList = function () {
+        $scope.search($scope.paginationConf.currentPage, $scope.paginationConf.itemsPerPage);
+    }
+
+    $scope.selectIds = [];//选中的ID集合
+    //用户勾选复选框
+    $scope.updateSelection = function ($event, id) {
+        if ($event.target.checked) {
+            $scope.selectIds.push(id);//push方法向集合添加元素
+        } else {
+            var index = $scope.selectIds.indexOf(id);//查找值的位置
+            $scope.selectIds.splice(index, 1);//参数1：移除的位置 参数2：移除的个数
+        }
+    }
+
+    $scope.jsonToString = function (jsonString, key) {
+
+        var json = JSON.parse(jsonString);
+        var value = "";
+
+        for (var i = 0; i < json.length; i++) {
+
+            if (i > 0) {
+                value += ","
+            }
+            value += json[i][key]
+
+        }
+
+        return value;
+
+    }
+
+
+});
